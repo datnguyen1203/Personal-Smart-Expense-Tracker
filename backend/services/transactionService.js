28eb2e087094ae2da2cfbbc3708b2b8ed37f903f
datnguyen12032003 <123805471+datnguyen1203@users.noreply.github.com>
2026-02-27 02:45:46 +0700

    Update transactionService.js

--- backend/services/transactionService.js
+++ backend/services/transactionService.js
@@ -1,4 +1,4 @@
-const Transaction = require('../models/Transaction');
+const Transaction = require('../models/transaction');
 
 const getAllTransactions = async () => {
     return await Transaction.find().sort({ date: -1 });
